f437c2a9424bc683210469aa2f7b7a31c1f1b8fc
Pedro Henrique <phlgasquez@gmail.com>
2024-04-26 13:49:22 -0300

    cors update

--- desafio-backend/Program.cs
+++ desafio-backend/Program.cs
@@ -1,7 +1,22 @@
+using desafio_backend;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.AddArchitectures();
 
+builder.Services.AddCors(
+    x=>x.AddPolicy(
+        Configuration.CorsPolicyName,
+        policy 
+            => 
+            policy.WithOrigins([Configuration.BackendUrl,Configuration.FrontendUrl])
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials()
+        ));
+
 var app = builder.Build();
+
+app.UseCors(Configuration.CorsPolicyName);
 app.MapRoutes();
 app.UseArchitectures();
 
