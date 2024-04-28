f91408cba6ab0c78b3a6b5a32b949070218f4fc6
Jaqueline Manzano <jaque_manzanos@hotmail.com>
2024-04-28 17:34:32 -0300

    Alter Configuration

--- desafio-backend/Configuration.cs
+++ desafio-backend/Configuration.cs
@@ -3,6 +3,6 @@ namespace desafio_backend;
 public static class Configuration
 {
     public static string CorsPolicyName = "wasm";
-    public static string BackendUrl = "http://localhost:5138/";
+    public static string BackendUrl = "https://maythefourthapp.azurewebsites.net/";
     public static string FrontendUrl = "";
 }
